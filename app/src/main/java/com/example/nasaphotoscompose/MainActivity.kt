a60675935730506ca359efac319061ef6bb1bcd8
Mercan Birer <mercan.birer@etiya.com>
2022-07-21 14:57:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/nasaphotoscompose/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.nasaphotoscompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.nasaphotoscompose.ui.theme.NasaPhotosComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
+//ItXjX7uV4euA35LozgIfhSIK1st5pYPfl1qAsFl0
+//https://api.nasa.gov/planetary/apod?api_key=ItXjX7uV4euA35LozgIfhSIK1st5pYPfl1qAsFl0
